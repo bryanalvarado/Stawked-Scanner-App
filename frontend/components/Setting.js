89d8c1e163aa4fcd609ddd33aafe2cb73546eda3
Alex-Furmuzan <73813436+Alex-Furmuzan@users.noreply.github.com>
2022-03-10 16:50:45 -0800

    Car-22 Settings Page

---
+++ frontend/components/Setting.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
+// /{...styles.settingCard, ...styles.navBarShadow, ...props.style}
+const Setting = (props) => {
+  return (
+    <TouchableHighlight
+      style={{ ...myStyles.setting, ...myStyles.topSetting, ...props.style }}
+      underlayColor={3}
+      onPress={props.onClick}
+    >
+      <View style={myStyles.settingName}>
+        <Text style={{ ...myStyles.settingNameText, ...props.textStyle }}>
+          {props.settingName}
+        </Text>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+const myStyles = StyleSheet.create({
+  setting: {
+    borderBottomWidth: 0.5,
+    borderBottomColor: "grey",
+    paddingVertical: 5,
+  },
+  settingName: {
+    paddingLeft: 10,
+  },
+  settingNameText: {
+    fontSize: 20,
+  },
+});
+
+export default Setting;
