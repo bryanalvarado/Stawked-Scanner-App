42a86d733fcfc1e63ac0f52a4d9b42002b71a5da
NoorDino <noordeen408@gmail.com>
2022-03-12 17:57:13 -0800

    fixed image bug

--- frontend/App.js
+++ frontend/App.js
@@ -20,6 +20,7 @@ const App = () => {
     // If the focused route is not found, we need to assume it's the initial screen
     // This can happen during if there hasn't been any navigation inside the screen
     // In our case, it's "Feed" as that's the first screen inside the navigator
+    //  ignore - testing commit 
     const routeName = getFocusedRouteNameFromRoute(route) ?? "Home";
 
     switch (routeName) {
