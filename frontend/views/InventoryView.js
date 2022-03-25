6326676eaad6236990dc75112fb7bd43de6bc3ef
Hector <63518338+mexi26@users.noreply.github.com>
2022-03-24 22:06:00 -0700

    CAR - 70 Styled Inventory Page
    Provided Styling to manipulate

--- frontend/views/InventoryView.js
+++ frontend/views/InventoryView.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { useAuth } from "../providers/AuthProvider";
 import { ListItem } from "react-native-elements";
+import styles from "../stylesheet";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 
 export function InventoryView({ navigation }) {
   const { user, projectData } = useAuth();
@@ -28,20 +30,66 @@ export function InventoryView({ navigation }) {
   }
 
   return (
-    <View>
-      {projectData.map((project) => (
-        <View key={project.name}>
-          <ListItem
-            onPress={() => onClickProject(project)}
-            bottomDivider
-            key={project.name}
-          >
-            <ListItem.Content>
-              <ListItem.Title>{project.name}</ListItem.Title>
-            </ListItem.Content>
-          </ListItem>
-        </View>
-      ))}
+    <View style={myStyles.screen}>
+      <View>
+        <ScrollView>
+          {projectData.map((project) => (
+            <View key={project.name}>
+
+              <TouchableOpacity onPress={() => onClickProject(project)} style={[myStyles.cardView, styles.navBarShadow]}>
+                <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+                  <View>
+                    <View >
+                      <Text style={{fontWeight: 'bold', fontSize: 20}}>
+                        {project.name}
+                      </Text>
+                    </View>
+
+                    <View >
+                      <Text style={{color: 'gray'}}>
+                        {"5"} items
+                      </Text>
+                    </View>
+                  </View>
+
+                  <View>
+                    <Text style={{fontSize: 20, color: 'gray'}}>
+                      {'>'}
+                    </Text>
+                  </View>
+
+
+                </View>
+              </TouchableOpacity>
+
+              {/* <ListItem
+                onPress={() => onClickProject(project)}
+                bottomDivider
+                key={project.name}
+              >
+                <ListItem.Content>
+                  <ListItem.Title>{project.name}</ListItem.Title>
+                </ListItem.Content>
+              </ListItem> */}
+            </View>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
+
+const myStyles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  cardView:{
+    borderWidth: 0,
+    backgroundColor: 'white',
+    padding: 20,
+    marginHorizontal: 10,
+    marginVertical: 5,
+    borderRadius: 20
+  }
+})
