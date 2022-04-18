590e27027547a511a4bb6750c3681feea04118db
noordeen abunamous <61770333+NoorDino@users.noreply.github.com>
2022-04-17 19:12:35 -0700

    home screen - detailed notifs, styling

---
+++ frontend/model/Notifications.js
@@ -0,0 +1,30 @@
+const NotificationsList = [
+    
+    "2 gallons of milk added to inventory"
+    ,
+    "Luncheables added to inventory"
+    ,
+    "Halal meat added to inventory"
+    ,
+    "Gatorade removed from inventory"
+    ,
+    "Water added to inventory"
+    ,
+    "Eggs added to inventory"
+    ,
+    "Potatoes removed from inventory"
+    ,
+    "2 gallons of milk added to inventory"
+    ,
+    "Luncheables added to inventory"
+    ,
+    "Halal meat added to inventory"
+    ,
+    "Gatorade removed from inventory"
+    ,
+    "Water added to inventory"
+    ,
+    "Eggs added to inventory"
+    ,
+    "Potatoes removed from inventory"
+];
